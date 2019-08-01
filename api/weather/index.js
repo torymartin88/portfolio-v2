0267e53152f014bceda5c7c0554206649eec9f0f
Tory Martin <tmartin@Torys-iMac.local>
2019-08-01 12:57:44 -0400

    Try

--- api/weather/index.js
+++ api/weather/index.js
@@ -2,13 +2,9 @@ const request = require('request')
 
 module.exports = (req, res) => {
     const url = `https://api.darksky.net/forecast/618ee46bab6b7bd5d7836bf24bf307d2/${req.body.lat},${req.body.long}`
-    let returnBody = null
 
     request(url, (err, res, body) => {
         if (err) { res.status(400).send('error') }
-        returnBody = body
-        console.log(body)
+        res.status(200).send(body);
     })
-    
-    res.status(200).send(returnBody);
 };
